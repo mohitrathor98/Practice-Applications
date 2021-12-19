289806ac7d5bc280acf2fe67394c1ee5fb26dae0
mohitrathor98 <mohitrathor98@github.com>
2021-12-19 17:17:06 +0530

    exception handling

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -7,6 +7,13 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 for (index, row) in data.iterrows():
     nato_dict[row['letter']] = row['code']
 
-user_name = input("What's the word? ").upper()
-nato_code = [nato_dict[l] for l in user_name]
+while(True):
+    try:
+        user_name = input("What's the word? ").upper()
+        nato_code = [nato_dict[l] for l in user_name]
+    except:
+        print("Sorry, only alphabets please")
+    else:
+        break
+    
 print(nato_code)
