1e2ef2e8e57c586d918397f95ad12d79d05f26bf
mohitrathor98 <mohitrathor98@github.com>
2021-12-21 22:25:38 +0530

    weekday motivation mail

---
+++ Birthday Wisher/main.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import smtplib
+import random
+
+def send_mail(message):
+    my_email = "mohitdemo7@gmail.com"
+    password = "asdf1234()"
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+
+        connection.starttls() 
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email, 
+            to_addrs="mohitdemo7@yahoo.com", 
+            msg=f"Subject:Weekday Motivation\n\n{message}"
+        )
+
+
+
+with open("quotes.txt", "r") as file:
+    quotes = file.readlines()
+
+current_day = dt.datetime.now().weekday()
+if current_day < 5:
+    send_mail(random.choice(quotes))
