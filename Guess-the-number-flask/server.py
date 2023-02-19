581b593931e6d65ab4cc7cdcf107957b98ff8156
mohitrathor98 <mohitrathor8@gmail.com>
2023-02-19 19:42:21 +0530

    skeleton for the logic

--- Guess-the-number-flask/server.py
+++ Guess-the-number-flask/server.py
@@ -1,15 +1,31 @@
 from flask import Flask, request
 import html_codes
+import random
 
 app = Flask(__name__)
 
+actualNum = -1
+
 @app.route('/')
 def landing():
+    global actualNum
+
+    actualNum = random.randint(0, 9)
     return html_codes.landing_body
 
 @app.route('/submit-form', methods=['POST'])
 def submit_form():
-    return request.form.get('choice-field')
+    global actualNum
+    value = int(request.form.get('choice-field'))
+    if value == actualNum:
+        #success page
+        return "Yep"
+    elif value > actualNum:
+        # Lower page
+        return "Go lower"
+    else:
+        # Higher page
+        return "Go higher"
 
 if __name__ == "__main__":
     app.run(debug=True)
