1fab9f5ccc984b760eca6685482d26e3236d9525
mohit <mohitrathor8@gmail.com>
2023-04-16 17:19:07 +0530

    Main Page completed

--- Flask-Jinja-Practice/blog-templating/main.py
+++ Flask-Jinja-Practice/blog-templating/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
-
+from post import Post
 
 app = Flask(__name__)
+postObj = Post()
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", allPosts=postObj.getData())
+
+@app.route("/dataById/<id>")
+def getDataById(id):
+    return render_template("post.html", post=postObj.getPostById(id))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Flask-Jinja-Practice/blog-templating/post.py
+++ Flask-Jinja-Practice/blog-templating/post.py
@@ -1,2 +1,19 @@
+import requests
 class Post:
-    pass
+    def __init__(self):
+        self.url = "https://api.npoint.io/80b7a3998e15140acf88"
+        self.data = None
+        
+    def getData(self):
+        res = requests.get(self.url)
+        print("Response: ", res.status_code)
+        self.data = res.json()
+        return self.data
+    
+    def getPostById(self, id):
+        if self.data is None:
+            self.getData()
+        
+        for post in self.data:
+            if post["id"] == id:
+                return post
